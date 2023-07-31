5828d158f1798aac8c9e1ebabd76c5a9077086f4
youssefch2003 <medyoussef429@gmail.com>
2023-07-31 15:46:30 +0100

    first_java

---
+++ javaFundamentals/first_java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+		  System.out.println("My name is youssef\n" +
+                           "I am 20 years old\n" +
+                           "My hometown is Burbank, CA");
+                }
+}
